9cf2dbafecdb5efa665f551e26adb975797149d4
Holli Davis <hollidavis19@gmail.com>
2021-09-01 16:26:50 -0600

    castles controller/service done

--- Controllers/CastlesController.cs
+++ Controllers/CastlesController.cs
@@ -1,7 +1,92 @@
-namespace knightsapi.Controllers
+using System;
+using System.Collections.Generic;
+using castlesapi.Models;
+using castlesapi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace castlesapi.Controllers
 {
-    public class CastlesController
+  [ApiController]
+  [Route("/api/[controller]")]
+  public class CastlesController : ControllerBase
+  {
+    private readonly CastlesService _castlesService;
+
+    public CastlesController(CastlesService castlesService)
     {
-        
+      _castlesService = castlesService;
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Castle>> Get()
+    {
+      try
+      {
+        IEnumerable<Castle> castles = _castlesService.Get();
+        return Ok(castles);
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Castle> Get(int id)
+    {
+      try
+      {
+        Castle castle = _castlesService.Get(id);
+        return Ok(castle);
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<Castle> Create([FromBody] Castle newCastle)
+    {
+      try
+      {
+        Castle castle = _castlesService.Create(newCastle);
+        return Ok(castle);
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<Castle> Edit([FromBody] Castle updatedCastle, int id)
+    {
+      try
+      {
+        updatedCastle.Id = id;
+        Castle castle = _castlesService.Edit(updatedCastle);
+        return Ok(castle);
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<String> Delete(int id)
+    {
+      try
+      {
+        _castlesService.Delete(id);
+        return Ok("Successfully Deleted");
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+  }
 }
--- Services/CastlesService.cs
+++ Services/CastlesService.cs
@@ -1,7 +1,56 @@
+using System;
+using System.Collections.Generic;
+using knightsapi.Models;
+using knightsapi.Repositories;
+
 namespace knightsapi.Services
 {
-    public class CastlesService
+  public class CastlesService
+  {
+    private readonly CastlesRepository _repo;
+
+    public CastlesService(CastlesRepository repo)
     {
-        
+      _repo = repo;
     }
+
+    internal IEnumerable<Castle> Get()
+    {
+      return _repo.Get();
+    }
+
+    internal Castle Get(int id)
+    {
+      Castle castle = _repo.Get(id);
+      if (castle == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      return castle;
+    }
+
+    internal Castle Create(Castle newCastle)
+    {
+      Castle castle = _repo.Create(newCastle);
+      if (castle == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      return castle;
+    }
+
+    internal Castle Edit(Castle updatedCastle)
+    {
+      Castle original = Get(updatedCastle.Id);
+      // check each value on the incoming object, if it exits then allow it to continue, if it does not set it to the original value
+      updatedCastle.Name = updatedCastle.Name != null ? updatedCastle.Name : original.Name;
+      return _repo.Update(updatedCastle);
+    }
+
+    internal void Delete(int id)
+    {
+      Castle original = Get(id);
+      _repo.Delete(original.Id);
+    }
+  }
 }
