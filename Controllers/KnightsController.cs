ed6a257304374fc50d5b2494c65ab0c71160d605
Holli Davis <hollidavis19@gmail.com>
2021-09-01 16:24:53 -0600

    Knight controller & service done

--- Controllers/KnightsController.cs
+++ Controllers/KnightsController.cs
@@ -1,7 +1,92 @@
+using System;
+using System.Collections.Generic;
+using knightsapi.Models;
+using knightsapi.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace knightsapi.Controllers
 {
-    public class KnightsController
+  [ApiController]
+  [Route("/api/[controller]")]
+  public class KnightsController : ControllerBase
+  {
+    private readonly KnightsService _knightsService;
+
+    public KnightsController(KnightsService knightsService)
     {
-        
+      _knightsService = knightsService;
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Knight>> Get()
+    {
+      try
+      {
+        IEnumerable<Knight> knights = _knightsService.Get();
+        return Ok(knights);
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Knight> Get(int id)
+    {
+      try
+      {
+        Knight knight = _knightsService.Get(id);
+        return Ok(knight);
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<Knight> Create([FromBody] Knight newKnight)
+    {
+      try
+      {
+        Knight knight = _knightsService.Create(newKnight);
+        return Ok(knight);
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<Knight> Edit([FromBody] Knight updatedKnight, int id)
+    {
+      try
+      {
+        updatedKnight.Id = id;
+        Knight knight = _knightsService.Edit(updatedKnight);
+        return Ok(knight);
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<String> Delete(int id)
+    {
+      try
+      {
+        _knightsService.Delete(id);
+        return Ok("Successfully Deleted");
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+  }
 }
--- Services/KnightsService.cs
+++ Services/KnightsService.cs
@@ -1,7 +1,56 @@
+using System;
+using System.Collections.Generic;
+using knightsapi.Models;
+using knightsapi.Repositories;
+
 namespace knightsapi.Services
 {
-    public class KnightsService
+  public class KnightsService
+  {
+    private readonly KnightsRepository _repo;
+
+    public KnightsService(KnightsRepository repo)
     {
-        
+      _repo = repo;
     }
+
+    internal IEnumerable<Knight> Get()
+    {
+      return _repo.Get();
+    }
+
+    internal Knight Get(int id)
+    {
+      Knight knight = _repo.Get(id);
+      if (knight == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      return knight;
+    }
+
+    internal Knight Create(Knight newKnight)
+    {
+      Knight knight = _repo.Create(newKnight);
+      if (knight == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      return knight;
+    }
+
+    internal Knight Edit(Knight updatedKnight)
+    {
+      Knight original = Get(updatedKnight.Id);
+      // check each value on the incoming object, if it exits then allow it to continue, if it does not set it to the original value
+      updatedKnight.Name = updatedKnight.Name != null ? updatedKnight.Name : original.Name;
+      return _repo.Update(updatedKnight);
+    }
+
+    internal void Delete(int id)
+    {
+      Knight original = Get(id);
+      _repo.Delete(original.Id);
+    }
+  }
 }
